598dc4957454e8a808aacddbeeb25df6d8618d3d
renderhaf <dvir.akerman@gmail.com>
2018-04-12 00:14:25 -0700

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var socket = require("socket.io");
+var app = express();
+app.use(express.static(__dirname + '/static'));
+
+var users = [];
+var connections = [];
+
+app.get('/Alerter/', function (req, res) {
+  res.sendfile(__dirname + '/static/Alerter.html');
+});
+
+var server = app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
+});
+
+var io = socket(server);
+
+io.of("/Alerter").on("connection", function(socket){
+  connections.push(socket);
+  console.log("Connected to " + socket.id + " There are currently " + connections.length + " connections")
+
+  socket.on("msg", function(data){
+    console.log(data);
+    io.of("/Alerter").emit("msg",data);
+    console.log("Emmited " + data);
+  })
+
+  socket.on("disconnect", function(socket){
+    connections.splice(connections.indexOf(socket),1);
+    console.log("Disconnected, there are now " + connections.length + " connections");
+  });
+
+});
