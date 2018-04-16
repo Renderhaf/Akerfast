f85f1a0a4f7fcd784c97e7fd43314208449c4f87
Renderhaf <dvir.akerman@gmail.com>
2018-04-16 09:20:53 -0700

    added draw

--- app.js
+++ app.js
@@ -4,30 +4,39 @@ var app = express();
 app.use(express.static(__dirname + '/static'));
 
 var users = [];
-var connections = [];
 
+//web gateways
 app.get('/Alerter/', function (req, res) {
   res.sendFile(__dirname + '/static/Alerter.html');
+});
+app.get('/Draw/', function (req, res) {
+  res.sendFile(__dirname + '/static/Draw.html');
 });
 
 var server = app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 5000!');
 });
 
+//Socket Listeners
 var io = socket.listen(server);
-
 io.of("/Alerter").on("connection", function(socket){
-  connections.push(socket);
-  console.log("Connected to " + socket.id + " There are currently " + connections.length + " connections")
-
   socket.on("msg", function(data){
     console.log(data);
     io.of("/Alerter").emit("msg",data);
     console.log("Emmited " + data);
   })
+});
+io.of("/Draw").on("connection", function(socket){
+  console.log("New Connection To Draw");
 
-  socket.on("disconnect", function(socket){
-    connections.splice(connections.indexOf(socket),1);
-    console.log("Disconnected, there are now " + connections.length + " connections");
-  });
+  socket.on("newstroke", function(data){
+    console.log(data);
+    if (data[1].length != 0){
+      io.of("/Draw").emit("newstroke",data);
+      console.log("Emmited to draw");
+    }
+  })
+  socket.on("disconnect",function(){
+    console.log("Disconnected");
+  })
 });
